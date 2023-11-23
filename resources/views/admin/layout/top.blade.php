6697151f1a025c4dab509a55ff0bfabe2a17be32
yuliaanandasiregar7 <yuli21073ti@student.nurulfikri.ac.id>
2023-11-23 09:11:47 +0700

    otentikasi login

--- resources/views/admin/layout/top.blade.php
+++ resources/views/admin/layout/top.blade.php
@@ -189,10 +189,15 @@
                 Activity Log
             </a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+            <!-- <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                 Logout
-            </a>
+            </a> -->
+            <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
         </div>
     </li>
 
